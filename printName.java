8ad17db7b942727534d6dd01408f59d55dea323c
chavhanindrayani <chavhanindrayani2002@gmail.com>
2025-03-25 10:16:36 +0530

    Add files via upload

---
+++ printName.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class printName
+{
+	public static void main(String args[])
+	{
+		String name;
+		int age;
+
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Enter the Name :-");
+		name=s.nextLine();
+		System.out.println("Enter the Age :-");
+		age=s.nextInt();
+	}
+}
