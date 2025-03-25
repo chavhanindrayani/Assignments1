2a52663bbaea0d3f00a4754dc46695933ccd8059
chavhanindrayani <chavhanindrayani2002@gmail.com>
2025-03-25 10:18:57 +0530

    Add files via upload

---
+++ AddNo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class AddNo
+{
+	public static void main(String args[])
+	{
+	int number1 = 2;
+	int number2 = 3;
+
+    int result = number1 + number2;
+	System.out.println("Addition of two number :- " + result);
+    }
+}
